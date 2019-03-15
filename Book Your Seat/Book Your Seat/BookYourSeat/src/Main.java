a9f843c00a5c6a2904af5e5fda722ffa96280b83
ChamodKanishka <chamodkanishka77@gmail.com>
2019-03-15 16:26:04 +0530

    movie ticket booking

---
+++ Book Your Seat/Book Your Seat/BookYourSeat/src/Main.java
@@ -0,0 +1,27 @@
+
+import javax.swing.JFrame;
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author Moraaa
+ */
+public class Main {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        Login login = new Login();
+        login.setTitle("Login");
+        login.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        login.setVisible(true);
+    }
+    
+}
